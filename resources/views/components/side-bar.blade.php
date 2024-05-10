ba2d5b493d319405bd7249a6589ad68f566e5630
Glen Brian G. Decado <glen.decado@gmail.com>
2024-05-11 04:34:11 +0800

    side bar

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-700 w-64 h-screen flex flex-col items-center']) }}>
+<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-700 w-64 h-screen hidden md:flex flex-col items-center']) }}>
     {{$slot}}
 </div>
